dd02adca6bc787f24351fedd894119ca89f24dab
biswascom <abjessbd@gmail.com>
2023-01-29 21:26:44 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,60 @@
+// var applePrice = 34;
+// var mangoPrice = 49;
+// applePrice -= 4;
+// mangoPrice++;
+
+// var inTotal = applePrice + mangoPrice;
+
+// console.log (inTotal);
+
+// var price1 = 0.1;
+// var price2 = 0.2;
+// var total = price1 + price2;
+
+// total = total.toFixed(1);
+// total = parseFloat(total);
+// console.log (total);
+
+// Fruits buy calculation - 01
+
+var monGaveMoney = 1000;
+var buyfruits = 700;
+var returnTaka = monGaveMoney - buyfruits;
+console.log ("Shopkeeper will give him " + returnTaka + " taka");
+
+// average marks calculation - 02
+
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = mathematics + biology + chemistry + physics + bangla;
+
+var average = totalMarks / 5;
+
+average = average.toFixed (2);
+
+average = parseFloat (average);
+
+console.log ("Average marks of those subjects is " + average);
+
+// string concatination provlem solving - 03
+
+var teachersGaveOne = "I am going to be";
+var teachersGaveTwo = "an awesome web developer"
+
+var output = teachersGaveOne + " " + teachersGaveTwo;
+
+console.log (output);
+
+// Sarah calculates the remainder - 04
+
+var gaveTheNumberIs = 119;
+var dividedNumberIs = 5;
+var remainder = gaveTheNumberIs % dividedNumberIs;
+
+console.log ("The remainder number is " + remainder);
+
+console.log (typeof remainder);
